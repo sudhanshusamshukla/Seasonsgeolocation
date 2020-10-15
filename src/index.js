e3ccdebe45fea9c966a83204fe2f73485a25f371
Sudhanshu Shukla <sudhanshu.shukla@outlook.com>
2020-10-15 15:14:47 +0530

    ReactSeason sample example -1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+
+//     );
+
+//     return (
+//         <div>
+//            Latitude : 
+//         </div>
+//     );
+// };
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //we called setState to update state
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => console.log(err)
+
+        );
+    }
+
+    //react says we have to define render
+    render() {
+        return <div> Latitude : {this.state.lat}</div>;
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
